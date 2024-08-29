af9a36956f47a30904aff6831279b04c5e65f3a2
samckp <sam.mca.liet@gmail.com>
2024-08-29 22:55:10 +0530

    jpa code implemented

--- src/main/java/com/sam/jpa/repository/CustomerRepository.java
+++ src/main/java/com/sam/jpa/repository/CustomerRepository.java
@@ -1,7 +1,6 @@
 package com.sam.jpa.repository;
 
 import com.sam.jpa.dao.Customer;
-import com.sam.jpa.dto.IOrdersOutput;
 import com.sam.jpa.dto.OrderResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,5 +12,6 @@ public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     @Query("select new com.sam.jpa.dto.OrderResponse(c.name, p.productName, p.price) from Customer c LEFT JOIN c.products p")
     public List<OrderResponse> getOrderInfo();
 
-
+    @Query(value = "select c.name as cName, c.email as email  from customer c limit 1", nativeQuery = true)
+    public List<Object> getCustomerDetail();
 }
